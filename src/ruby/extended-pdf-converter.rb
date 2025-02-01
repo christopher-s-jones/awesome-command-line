e0dba7e12c1b9d38ab0ac734e4cc0868da5c0237
Christopher Jones <chris@christopherjones.co>
2025-02-01 07:48:24 -0700

    Update the header ornament an location:
    Change the ornament to have sparkles left and right,
    and modify the PDF extension to place the ornament
    next to the chapter signifier.

--- src/ruby/extended-pdf-converter.rb
+++ src/ruby/extended-pdf-converter.rb
@@ -1,18 +1,44 @@
+
 class PDFConverterCustomChapterTitle < (Asciidoctor::Converter.for 'pdf')
     register_for 'pdf'
-  
-    # def ink_chapter_title node, title, opts = {}
-    #   move_down cursor * 0.25
-    #   ink_heading title, (opts.merge align: :center, text_transform: :uppercase)
-    #   stroke_horizontal_rule 'DDDDDD', line_width: 1
-    #   move_down theme.block_margin_bottom
-    #   theme_font :base do
-    #     layout_prose 'Custom text here, maybe a chapter preamble.'
-    #   end
-    #   start_new_page
-    # end
 
   def ink_chapter_title node, title, opts = {}
+  
+    require 'logger'
+
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::DEBUG
+    logger.formatter = proc do |severity, datetime, progname, msg| 
+      "#{severity} - #{msg}\n" 
+    end
+  
+    # Conditionally set the amount to relocate the cursor
+    # based on the section title
+    case title
+    when /Preface/
+      move_x = 326
+      move_y = 162
+    when /Acknowledgements/
+      move_x = 250
+      move_y = 162
+    when /Chapter/
+      move_x = 314
+      move_y = 191
+    when /Appendix A/
+      move_x = 126
+      move_y = 162
+    when /Appendix B/
+      move_x = 141
+      move_y = 162
+    when /Index/
+      move_x = 321
+      move_y = 161
+    else
+      logger.debug "No match for #{title}"
+      move_x = 300
+      move_y = 162
+    end
+
     title_array = title.split('.')
     if title_array[1] != nil
       title_array[1].index(':') != nil ? 
@@ -30,7 +56,9 @@ class PDFConverterCustomChapterTitle < (Asciidoctor::Converter.for 'pdf')
     )
 
     # "Chapter 2"
+    logger.debug "BEGIN #{title} -----------------------"
     signifier = title_array[0].upcase
+    logger.debug "Writing #{signifier}. Cursor is now: #{cursor}"
     text(signifier, 
       align: :right,
       text_transform: 'uppercase',
@@ -39,19 +67,32 @@ class PDFConverterCustomChapterTitle < (Asciidoctor::Converter.for 'pdf')
       color: '292524',
       size: '0.75em'
     )
-
+    logger.debug "Wrote #{signifier}. Cursor is now: #{cursor}"
+  
     stroke_horizontal_rule '292524' , line_width: 0.5
-    # stroke_color 'FF0000'
-    # stroke do
-    #   vertical_line 100, 400, at: 300
-    # end
-    ink_heading new_title, (opts.merge)
-    move_up 192
-    image "./images/logo-header-ornament.svg", fit: [15.7, 19.7]
-    move_down 185
+    logger.debug "Wrote HZ rule. Cursor is now: #{cursor}"
 
-    if command_list != nil
-      move_up 135
+    ink_heading new_title, (opts.merge)
+    logger.debug "Wrote #{new_title} Cursor is now: #{cursor}"
+
+    move_up move_y
+    logger.debug "Moved up #{move_y}. Cursor is now: #{cursor}"
+
+    # The ornament is 280x128 pixels, so downsize it to 11%
+    # to fit on the heading line
+    img_width = 280 * 0.11
+    img_height = 128 * 0.11
+    image "./images/logo-header-ornament.svg", fit: [img_width, img_height], at: [move_x, cursor]
+    logger.debug "Wrote image. Cursor is now: #{cursor}"
+
+    move_down 155
+    logger.debug "Moved down 155. Cursor is now: #{cursor}"
+    
+    if command_list != nil && command_list.size > 0
+      logger.debug("Command list size: #{command_list.size}")
+      move_y = 105
+      move_up move_y
+      logger.debug "Moved up #{move_y}. Cursor is now: #{cursor}"
       text(command_list, 
         align: :right,
         font: 'PT Sans Narrow',
@@ -59,7 +100,11 @@ class PDFConverterCustomChapterTitle < (Asciidoctor::Converter.for 'pdf')
         color: '292524',
         size: '0.75em'
       )
-      move_down 135
+      logger.debug "Wrote #{command_list}. Cursor is now: #{cursor}"
+
+      move_down 85
+      logger.debug "Moved down 90. Cursor is now: #{cursor}"
     end
+    logger.debug "END #{title} -----------------------\n"
   end
 end
